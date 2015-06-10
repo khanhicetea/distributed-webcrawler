39a0efc556f68fd5735548c85136c54699aaffd5
KhanhIceTea <khanhicetea@gmail.com>
2015-06-10 01:03:50 -0400

    Basic crawler has worked

--- crawler.py
+++ crawler.py
@@ -3,7 +3,7 @@ import sys
 import yaml
 import rethinkdb as r
 import gearman
-import urllib2
+import requests
 
 parameter_file = open("parameters.yml", "r")
 parameters = yaml.load(parameter_file)
@@ -11,19 +11,23 @@ parameters = yaml.load(parameter_file)
 rethink = r.connect(parameters['rethinkdb_server']['host'], parameters['rethinkdb_server']['port']).repl()
 rethink.use(parameters['rethinkdb_server']['database'])
 raw_result_table = parameters['rethinkdb_server']['tables']['raw_result']
-
+crawler_headers = parameters['crawler_headers']
+print crawler_headers
 gm_worker = gearman.GearmanWorker(parameters['gearman_server']['hosts'])
 
 def task_listener_crawler(gearman_worker, gearman_job):
 	url = gearman_job.data
-	try:
-		response = urllib2.urlopen(url)
-		raw_data = response.read()
-		r.table(raw_result_table).insert({'url': url, 'raw': raw_data}).run(rethink)
-	except urllib2.HTTPError as e:
-		pass
-		
-	return "ok"
+	print "Crawling ", url
+	response = requests.get(url, crawler_headers)
+	if response.status_code == 200:
+		raw_data = response.text
+		if response.encoding != 'utf8':
+			raw_data = response.text.encode(response.encoding).decode('utf8')
+		r.table(raw_result_table).insert({'url': url, 'raw': raw_data, 'status': 200}).run(rethink)
+		return "ok"
+	else:
+		r.table(raw_result_table).insert({'url': url, 'status': response.status_code}).run(rethink)
+	return "fail"
 
 def main(argv):
 	# Main code here
