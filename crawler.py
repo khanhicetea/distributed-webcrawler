49e5ff9c4b814126319b36cba0f4509a58882583
KhanhIceTea <khanhicetea@gmail.com>
2015-06-11 06:44:20 -0400

    Normalize URL before check in URL Frontier

--- crawler.py
+++ crawler.py
@@ -3,7 +3,12 @@ import sys
 import yaml
 import rethinkdb as r
 import gearman
+import redis
+from pybloomd import BloomdClient
+import re
+import urlparse
 import requests
+import urlnorm
 
 parameter_file = open("parameters.yml", "r")
 parameters = yaml.load(parameter_file)
@@ -11,12 +16,26 @@ parameters = yaml.load(parameter_file)
 rethink = r.connect(parameters['rethinkdb_server']['host'], parameters['rethinkdb_server']['port']).repl()
 rethink.use(parameters['rethinkdb_server']['database'])
 raw_result_table = parameters['rethinkdb_server']['tables']['raw_result']
-crawler_headers = parameters['crawler_headers']
-print crawler_headers
+redis_client = redis.Redis(parameters['redis_server']['host'])
 gm_worker = gearman.GearmanWorker(parameters['gearman_server']['hosts'])
+bloom_client = BloomdClient(parameters['bloomd_servers'])
+url_frontier = bloom_client.create_filter('url_frontier')
+linkregex = re.compile('<a\s*href=[\'|"](.*?)[\'"].*?>')
+crawler_headers = parameters['crawler_headers']
+
+def url_pre_norm(link, base):
+	if link.startswith('/'):
+		link = 'http://' + base[1] + link
+	elif link.startswith('#'):
+		link = 'http://' + base[1] + base[2] + link
+	elif not link.startswith('http'):
+		link = 'http://' + base[1] + '/' + link
+	return link
 
 def task_listener_crawler(gearman_worker, gearman_job):
 	url = gearman_job.data
+	url_frontier.add(url)
+	urls = urlparse.urlparse(url)
 	print "Crawling ", url
 	response = requests.get(url, crawler_headers)
 	if response.status_code == 200:
@@ -24,6 +43,14 @@ def task_listener_crawler(gearman_worker, gearman_job):
 		if response.encoding != 'utf8':
 			raw_data = response.text.encode(response.encoding).decode('utf8')
 		r.table(raw_result_table).insert({'url': url, 'raw': raw_data, 'status': 200}).run(rethink)
+
+		links = linkregex.findall(raw_data)
+		for link in (links.pop(0) for _ in xrange(len(links))):
+			pre_norm_url = url_pre_norm(link, urls)
+			norm_url = urlnorm.norm(pre_norm_url)
+			if url_frontier.add(norm_url):
+				print "Add ", norm_url, " to redis queue"
+				redis_client.sadd("enqueued_urls", norm_url)
 		return "ok"
 	else:
 		r.table(raw_result_table).insert({'url': url, 'status': response.status_code}).run(rethink)
